253d2d67cda1970fa3e9bfffa696c992daac0949
Daniel Segel <dsegel@gmail.com>
2023-10-01 09:18:30 -0700

    updated trackball.ino for Fn+Trackball scrolls

--- Code/uconsole_keyboard/trackball.ino
+++ Code/uconsole_keyboard/trackball.ino
@@ -1,6 +1,6 @@
 /*
- * clockworkpi devterm trackball 
- */
+   clockworkpi devterm trackball
+*/
 #include "keys_io_map.h"
 
 #include <stdint.h>
@@ -17,8 +17,8 @@
 #include "math.h"
 
 
- 
-enum Axis: uint8_t {
+
+enum Axis : uint8_t {
   AXIS_X,
   AXIS_Y,
   AXIS_NUM,
@@ -54,20 +54,21 @@ static void interrupt( ) {
   if (AXIS == AXIS_X) {
     glider[AXIS_X].update(vx, std::sqrt(rateMeter[AXIS_X].delta()));
     glider[AXIS_Y].updateSpeed(vy);
-    
+
   } else {
     glider[AXIS_X].updateSpeed(vx);
     glider[AXIS_Y].update(vy, std::sqrt(rateMeter[AXIS_Y].delta()));
 
   }
 
- 
+
 }
- 
+
 void trackball_task(DEVTERM*dv) {
   int8_t x = 0, y = 0, w = 0;
   noInterrupts();
-  const auto mode = dv->state->moveTrackball();
+  //const auto mode = dv->state->moveTrackball();
+  const auto mode = dv->Keyboard_state.fn_on == 0 ? TrackballMode::Mouse : TrackballMode::Wheel;
   if (lastMode != mode) {
     rateMeter[AXIS_X].expire();
     rateMeter[AXIS_Y].expire();
@@ -77,54 +78,54 @@ void trackball_task(DEVTERM*dv) {
     rateMeter[AXIS_X].tick(dv->delta);
     rateMeter[AXIS_Y].tick(dv->delta);
   }
-  lastMode = mode; 
-  
-  switch(mode){
-    case TrackballMode::Mouse: {
-      const auto rX = glider[AXIS_X].glide(dv->delta);
-      const auto rY = glider[AXIS_Y].glide(dv->delta);
-      x = rX.value;
-      y = rY.value;
-      if (rX.stopped) {
-        glider[AXIS_Y].stop();
-      }
-      if (rY.stopped) {
-        glider[AXIS_Y].stop();
-      }
+  lastMode = mode;
 
-      break;
-    }
-    case TrackballMode::Wheel: {
-      wheelBuffer += distances[AXIS_Y];
-      w = wheelBuffer / WHEEL_DENOM;
-      wheelBuffer -= w * WHEEL_DENOM;
-      if(w != 0){
-        dv->state->setScrolled();
+  switch (mode) {
+    case TrackballMode::Mouse: {
+        const auto rX = glider[AXIS_X].glide(dv->delta);
+        const auto rY = glider[AXIS_Y].glide(dv->delta);
+        x = rX.value;
+        y = rY.value;
+        if (rX.stopped) {
+          glider[AXIS_Y].stop();
+        }
+        if (rY.stopped) {
+          glider[AXIS_Y].stop();
+        }
+
+        break;
       }
-      break;
-    }
+    case TrackballMode::Wheel: {
+        wheelBuffer += distances[AXIS_Y];
+        w = wheelBuffer / WHEEL_DENOM;
+        wheelBuffer -= w * WHEEL_DENOM;
+        if (w != 0) {
+          dv->state->setScrolled();
+        }
+        break;
+      }
   }
   distances[AXIS_X] = 0;
   distances[AXIS_Y] = 0;
   interrupts();
 
-  if(x !=0 || y != 0 || -w!=0) {
+  if (x != 0 || y != 0 || -w != 0) {
     dv->Mouse->move(x, y, -w);
   }
- 
+
 }
 
 
-void trackball_init(DEVTERM*dv){
+void trackball_init(DEVTERM*dv) {
 
   pinMode(LEFT_PIN, INPUT);
   pinMode(UP_PIN, INPUT);
   pinMode(RIGHT_PIN, INPUT);
   pinMode(DOWN_PIN, INPUT);
 
-  attachInterrupt(LEFT_PIN, &interrupt<AXIS_X,-1> , ExtIntTriggerMode::CHANGE);
+  attachInterrupt(LEFT_PIN, &interrupt < AXIS_X, -1 > , ExtIntTriggerMode::CHANGE);
   attachInterrupt(RIGHT_PIN, &interrupt<AXIS_X, 1>, ExtIntTriggerMode::CHANGE);
-  attachInterrupt(UP_PIN, &interrupt<AXIS_Y, -1>, ExtIntTriggerMode::CHANGE);
+  attachInterrupt(UP_PIN, &interrupt < AXIS_Y, -1 >, ExtIntTriggerMode::CHANGE);
   attachInterrupt(DOWN_PIN, &interrupt<AXIS_Y, 1>, ExtIntTriggerMode::CHANGE);
-  
+
 }
